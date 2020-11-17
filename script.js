417bb2db5c4f9a515cdcb481f34c3078d032afad
Megan Lavallee <megan.lavallee@yahoo.com>
2020-11-16 20:13:28 -0700

    night update

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-var queryURL= http://api.weatherbit.io/v2.0/current
+$("#search-btn").on("click", function () {
+    getWeather();
+    getCity();
+});
+
+// ajax call to "getWeather()" and set variables needed to run "getWeather()"
+function getWeather() {
+    var queryURL = "http://api.weatherbit.io/v2.0/current";
+    $.ajax({
+        url: queryURL,
+        type: "GET",
+    }).then(() => {
+        console.log(dinner);
+
+
+    });
+}
+
+const form = document.querySelector(".top-banner form");
+ 
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  const inputVal = input.value;
+
+msg.textContent = "";
+form.reset();
+input.focus();
+});
