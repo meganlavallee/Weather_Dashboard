69a7c7a31361e58db112181f7aec397dadb5f09f
Megan Lavallee <megan.lavallee@yahoo.com>
2020-12-28 21:31:47 -0700

    5 day forecast, js second api call

--- script.js
+++ script.js
@@ -1,80 +1,84 @@
 let city_name = "";
 
 $("#search-btn").on("click", function (e) {
-    e.preventDefault();
-    // getCity();
-    city_name = $("#Search").val().trim();
-    getWeather();
+  e.preventDefault();
+  // getCity();
+  city_name = $("#Search").val().trim();
+  $(".jumbotron").empty();
+
+  getWeather();
+  console.log(city_name);
 });
-console.log(city_name)
+
+// $("#saved-cities").on("click", function (e) {
+//     if (!e.target.matches(".savedCities")) return;
+
+//     var saveCity = e.target.id;
+//     getWeatherDetails(saveCity, false);
+//     console.log(saveCity);
+// });
+
 // ajax call to "getWeather()" and set variables needed to run "getWeather()"
 function getWeather() {
-    const API_Key = "5cc23645c43dae5b8f5251a592a29a62"
+  const API_Key = "5cc23645c43dae5b8f5251a592a29a62";
+  // Current Weather
+  const queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city_name}&units=imperial&appid=${API_Key}`;
 
-    // Current Weather
-    var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city_name}&units=imperial&appid=${API_Key}`;
-    $.ajax({
-        url: queryURL,
-        type: "GET",
-    }).then((data) => {
-        console.log(data);
+  // AJAX Call for Data
+  $.ajax({
+    url: queryURL,
+    type: "GET",
+  }).then((data) => {
+    console.log(data);
 
-        // H3 Tag of Attribute Class with City Temperature and Numbers from Call
-        let temperature = $("<h3>").attr("class", "city_temperature").text("Temperature: " + data.main.temp + "F");
-        let humidity = $("<h3>").attr("class", "city_humidity").text("Humidity: " + data.main.humidity + "%")
-        let speed = $("<h3>").attr("class", "wind_speed").text("Wind Speed: " + data.wind.speed + "mph")
-        let feelslike = $("<h3>").attr("class", "wind_speed").text("Feels Like: " + data.main.feels_like + "F")
-        //   let description = $("<h3>").attr("class", "description").text("description: " + data.weather.main.description + "")
+    //     paradameters.lon = data.coord.lon;
+    // H3 Tag of Attribute Class with City Temperature and Numbers from Call
+    let temperature = $("<h3>")
+      .attr("class", "city_temperature")
+      .text("Temperature: " + data.main.temp + "F");
+    let humidity = $("<h3>")
+      .attr("class", "city_humidity")
+      .text("Humidity: " + data.main.humidity + "%");
+    let speed = $("<h3>")
+      .attr("class", "wind_speed")
+      .text("Wind Speed: " + data.wind.speed + "mph");
+    let feelslike = $("<h3>")
+      .attr("class", "wind_speed")
+      .text("Feels Like: " + data.main.feels_like + "F");
+    //   let description = $("<h3>").attr("class", "description").text("description: " + data.weather.main.description + "")
 
-        // Append and Post Everything On the Webpage
-        $(".jumbotron").append(temperature, humidity, speed, feelslike)
+    // Append and Post Everything On the Webpage
+    $(".jumbotron").append(temperature, humidity, speed, feelslike);
 
-    });
+    var lat = data.coord.lat;
+    var lon = data.coord.lon;
+
+    let secondqueryURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude={part}&appid=${API_Key}`;
 
     // 7 Day: Temp, Cloud Cover, Humidity
-    function secondRequest() {
-        let secondqueryURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={part}&appid=${API_Key2}`;
-
-        // LAT AND Long
-        let paradameters = { appid: "5cc23645c43dae5b8f5251a592a29a62" };
-        paradameters.lat = data.coord.lat;
-        paradameters.lon = data.coord.lon;
-
-        // Return Second Request URL
-        return secondqueryURL + $.param(paradameters)
-    }
-
-    // Putting Together
-    let secondqueryURL = secondRequest();
+    console.log(lat, lon);
 
     // Second Request
-        $.ajax({
-            url: secondqueryURL,
-            type: "GET",
-        }).then((response) => {
-            console.log(response);
+    $.ajax({
+      method: "GET",
+      url: secondqueryURL,
+    }).then((response) => {
+      console.log(response);
 
+      // Temperature, Humidity
+      for (let i = 1; i < 6; i++) {
+        $("#forecast" + [i]).append(
+          $("<h2>")
+            .attr("class", "description")
+            .text("Temperature: " + response.daily[i].temp.day + "F")
+        );
 
-            // H3 Tag of Attribute Class with City Temperature and Numbers from Call
-            let temperature7 = $("<h2>").attr("class", "temperature").text("Temperature: " + response.main.temp + "F");
-            let humidity7 = $("<h2>").attr("class", "humidity").text("Humidity: " + response.main.humidity + "%")
-            let cloudcover = $("<h2>").attr("class", "cloud_cover").text("Cloud Cover: " + response.main.clouds + "")
-
-
-            // // Append and Post Everything On the Webpage
-            $(".carousel").append(temperature7, humidity7, cloudcover)
-
-        });
+        $("#forecast" + [i]).append(
+          $("<h2>")
+            .attr("class", "humidity")
+            .text("Humidity: " + response.daily[i].humidity + "%")
+        );
+      }
+    });
+  });
 }
-
-
-// const form = document.querySelector(".top-banner form");
-
-// form.addEventListener("submit", e => {
-//   e.preventDefault();
-//   const inputVal = input.value;
-
-// msg.textContent = "";
-// form.reset();
-// input.focus();
-// });
